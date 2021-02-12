704a43db9d7151af57aa7fdbd075043df6e4db80
Malik Elate <Malik.e1955@gmail.com>
2021-02-12 10:58:13 -0600

    updating calc function

--- src/Components/DayProfit/DayProfit.tsx
+++ src/Components/DayProfit/DayProfit.tsx
@@ -14,62 +14,13 @@ export const DoorDash: React.FC<Props> = (props) => {
   const [singleMessage, setSingleMessage] = useState<string>("");
 
   useEffect(() => {
-    // Update the document title using the browser API 
-    console.log(singleMessage)
     setMessageFunction(); 
-    console.log(singleMessage)
   }, []);
 
   const setMessageFunction = () => { 
     if ((0 < singleMiles) && (singleMiles <= 2)  && (singlePay > 4)){
         setSingleMessage('This dash is unprofitable');
-    } else if ((0 >= singleMiles) && (singleMiles > 2)) {
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-     } else if ((2 < singleMiles) && (singleMiles <= 3)  && (singlePay > 6)){
-        setSingleMessage('This dash is profitable!');
-        return "end function";
-      } else if ((2 >= singleMiles) && (singleMiles > 3)) {
-        setSingleMessage('This dash is unprofitable');   
-        return "end function";
-      } else if ((3 < singleMiles) && (singleMiles <= 4)  && (singlePay > 8)){
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((3 >= singleMiles) && (singleMiles > 4)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((4 < singleMiles) && (singleMiles <= 5)  && (singlePay > 11)){
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((4 >= singleMiles) && (singleMiles > 5)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((5 < singleMiles) && (singleMiles <= 6)  && (singlePay > 15)){
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((5 >= singleMiles) && (singleMiles > 6)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((6 < singleMiles) && (singleMiles <= 7)  && (singlePay > 18)){
-      setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((6 >= singleMiles) && (singleMiles > 7)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((7 < singleMiles) && (singleMiles <= 9)  && (singlePay > 26)){
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((7 >= singleMiles) && (singleMiles > 9)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((9 < singleMiles) && (singleMiles <= 12)  && (singlePay > 35)){
-        setSingleMessage('This dash is unprofitable');
-      return "end function";
-      } else if ((9 >= singleMiles) && (singleMiles > 12)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if ((13 < singleMiles) && (singleMiles <= 16)  && (singlePay > 50)){
-        setSingleMessage('This dash is unprofitable');
-        return "end function";
-      } else if ((13 >= singleMiles) || (singleMiles > 16)) {
-        setSingleMessage('This dash is unprofitable');
-      } else if (16 < singleMiles) {
-        setSingleMessage('DoorDash does not assign dashes of this distance');
-      }
+    }
   }
   return (
     <div className="calc-page">
@@ -86,7 +37,7 @@ export const DoorDash: React.FC<Props> = (props) => {
           />
           <div className="mui-slider-div">
             <input
-            className='calc-input'
+            className='calc-slider'
             type='range' 
             aria-labelledby="discrete-slider-small-steps"
             step={0.5}
@@ -111,6 +62,7 @@ export const DoorDash: React.FC<Props> = (props) => {
           />
           <div className="mui-slider-div">
             <input
+            className='calc-slider'
             type='range' 
             aria-labelledby="discrete-slider-small-steps"
             step={0.5}
@@ -125,12 +77,8 @@ export const DoorDash: React.FC<Props> = (props) => {
             />
           </div>
         </div> 
-        <div>
-          <p>Status: {singleMessage}</p>
-        </div>
-        <div>
-          <p>singlePay: ${singleAdjustedPay}</p>
-        </div>
+        <p>Status: {singleMessage}</p>
+        <p>singlePay: ${singleAdjustedPay}</p>
         <p> Roundtrip time: {singleMiles>0? 15+singleMiles*6 : 0} minutes</p>
         <WeeklyProfit singleProfits={singleAdjustedPay}/>
       </div>
