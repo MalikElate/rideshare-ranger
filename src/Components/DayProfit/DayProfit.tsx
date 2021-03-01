9b2ca955d824f04ccac1e9dd1fdb82face4f406e
Malik Elate <Malik.e1955@gmail.com>
2021-03-01 12:04:11 -0600

    fixed the set message function

--- src/Components/DayProfit/DayProfit.tsx
+++ src/Components/DayProfit/DayProfit.tsx
@@ -11,66 +11,102 @@ export const DoorDash: React.FC<Props> = (props) => {
   const [singleMiles, setSingleMiles] = useState<number>(0);
   const [singlePay, setSinglePay] = useState<number>(0);
   const [singleAdjustedPay, setSingleAdjustedPay] = useState<number>(0);
-  const [singleMessage, setSingleMessage] = useState<string>("");
+  const [singleMessage, setSingleMessage] = useState<string>('');
 
-  const setMessageFunction = () => { 
-    switch (singleMiles)  { 
+  useEffect(() => {
+    setMessageFunction(singleMiles, singlePay)
+  }, [singleMiles, singlePay]);
+
+  const setMessageFunction = (miles: number, pay: number) => { 
+    switch (miles)  { 
+      case 0: 
+        setSingleMessage('Deliveries must have some distance'); 
+        break; 
       case .5:  
       case 1: 
-        { 
-          console.log('.5 or 1'); 
-          break; 
-        }
       case 1.5:  
       case 2: 
-        { 
-          console.log('1.5 or 2'); 
-          break; 
+        if(singlePay >= 5.5) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
         }
+        break; 
       case 2.5: 
       case 3:
-         { 
-           console.log('2.5 or 3'); 
-           break; 
-         } 
-      case 3:
-      case 3.5:
-         { 
-           console.log('3 or 3.5'); 
-           break; 
-         }
-      case 4:
-      case 4.5:
-         { 
-           console.log('4 or 4.5'); 
-           break; 
-         }
-      case 5: 
-      case 5.5: 
-        {   
-          console.log('5 or 5.5'); 
-          break; 
+        if(singlePay >= 7) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
         }
+        break; 
+      case 3.5:
+      case 4:
+        if(singlePay >= 8) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+      case 4.5:
+      case 5:
+        if(singlePay >= 10) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+      case 4.5:
+      case 5:
+      case 5.5: 
+      case 6:
+        if(singlePay >= 12) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+      case 6.5:
+      case 7:
+      case 7.5:
+      case 8:
+        if(singlePay >= 15) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+      case 8.5:
+      case 9:
+      case 9.5:
+      case 10:
+      case 10.5:
+        if(singlePay >= 19) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+      case 11:
+      case 11.5:
+      case 12:
+      case 12.5:
+      case 13:
+      case 13.5:
+      case 14:
+      case 14.5:
+      case 15:
+        if(singlePay >= 27) { 
+          setSingleMessage('this dash is worth it'); 
+        } else { 
+          setSingleMessage('this dash is a waste of time'); 
+        }
+        break; 
+        default:
+          setSingleMessage('this dash is a waste of time'); 
+          break;
+      }
     }
-    // // any dash less than 5 dollars pay is unprofitable
-    // if (singlePay < 5) {
-    //   setSingleMessage('This dash is unprofitable');
-    //   return; 
-    // }
-    // // any dash further than 14 miles is unprofitable
-    // if (singleMiles > 14) {
-    //   setSingleMessage('This dash is unprofitable');
-    //   return; 
-    // }
-    // if (singlePay >= 5 && singleMiles < 3) {
-    //   setSingleMessage('This dash is profitable');
-    //   return; 
-    // } 
-    // if (singlePay >= 5 && singleMiles < 3) { 
-    //   setSingleMessage('This dash is profitable');
-    //   return; 
-    // }
-  }
   return (
     <>
     <DailyProfitHeader companyName={props.companyName}/>      
@@ -87,7 +123,6 @@ export const DoorDash: React.FC<Props> = (props) => {
             value={singleMiles} 
             onChange={(e)=>{
               setSingleMiles(Number(e.target.value)); 
-              setMessageFunction(); 
               setSingleAdjustedPay(Number(Number(singlePay - (singleMiles/25 * 2.2)).toFixed(2)));
             }}
           />
@@ -103,7 +138,6 @@ export const DoorDash: React.FC<Props> = (props) => {
             onChange={(e) => {
               setSingleMiles(Number(e.target.value)); 
               setSingleAdjustedPay(Number(Number(singlePay - (singleMiles/25 * 2.2)).toFixed(2)));
-              setMessageFunction();
             }}
             />
           </div>
@@ -117,7 +151,6 @@ export const DoorDash: React.FC<Props> = (props) => {
             value={singlePay} 
             onChange={(e)=>{
               setSinglePay(Number(e.target.value)); 
-              setMessageFunction(); 
               setSingleAdjustedPay(Number(Number(singlePay - (singleMiles/25 * 2.2)).toFixed(2)));
             }}
           />
@@ -133,7 +166,6 @@ export const DoorDash: React.FC<Props> = (props) => {
             onChange={(e) => {
               setSinglePay(Number(e.target.value));
               setSingleAdjustedPay(Number(Number(singlePay - (singleMiles/25 * 2.2)).toFixed(2)));
-              setMessageFunction();
             }}
             />
           </div>
